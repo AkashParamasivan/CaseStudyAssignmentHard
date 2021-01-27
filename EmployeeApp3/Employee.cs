4c3ddd3362ffbae4b2d42fe2883a01067984871b
admin <admin@DESKTOP-954FFPA>
2021-01-27 18:50:45 +0530

    Add project files.

---
+++ EmployeeApp3/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp3
+{
+	class Employee
+	{
+		int id, age;
+		string name;
+		double salary;
+		
+		public Employee()
+		{
+		}
+		public Employee(int id, int age, string name, double salary)
+		{
+			this.id = id;
+			this.age = age;
+			this.name = name;
+			this.salary = salary;
+		}
+		
+		public void TakeEmployeeDetailsFromUser()
+		{
+			Console.WriteLine("Please enter the employee ID");
+			id = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter the employee name");
+			name = Console.ReadLine();
+			Console.WriteLine("Please enter the employee age");
+			age = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Please enter the employee salary");
+			salary = Convert.ToDouble(Console.ReadLine());
+		}
+		public void DisplayMenu()
+		{
+			Console.WriteLine("Please enter the option");
+			Console.WriteLine("1. Add an employee");
+			Console.WriteLine("2. Modify an employee detail");
+			Console.WriteLine("3. Print all employee's details");
+			Console.WriteLine("4. Print an employee detail");
+			Console.WriteLine("5. Exit");
+			
+
+		}
+		//public void AddEmployeeDetails()
+		//{ 
+
+		//employees.Add(Id, Tuple.Create(Name, Age, Salary));
+
+  //      }
+		public override string ToString()
+		{
+			return "Employee ID : " + id + "\nName : " + name + "\nAge : " + age +
+		   "\nSalary : " + salary;
+		}
+		public int Id { get => id; set => id = value; }
+		public int Age { get => age; set => age = value; }
+		public string Name { get => name; set => name = value; }
+		public double Salary { get => salary; set => salary = value; }
+	}
+}
---
+++ EmployeeApp3/EmployeeMenu.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp3
+{
+	class EmployeeMenu
+	{
+		static void Main(string[] args)
+		{
+			   
+			int Checker = 1;
+
+			bool flag = true;
+			IDictionary<int, Tuple<string, int, double>> employees = new Dictionary<int, Tuple<string, int, double>>();
+			Employee em = new Employee();
+			
+				while (flag)
+				{
+
+
+					em.DisplayMenu();
+					int ch = Int32.Parse(Console.ReadLine());
+
+
+
+					switch (ch)
+					{
+						//Add employee
+						case 1:
+							{
+								try
+								{
+									do
+									{
+										em.TakeEmployeeDetailsFromUser();
+
+										employees.Add(em.Id, Tuple.Create(em.Name, em.Age, em.Salary));
+										Console.WriteLine("To continue entering employee details enter 1, to exit enter 0");
+										Checker = Convert.ToInt32(Console.ReadLine());
+									} while (Checker == 1);
+								}
+								catch (Exception ex)
+								{
+									Console.WriteLine("------Give proper Input-------", ex);
+								}
+
+
+							}
+							break;
+
+
+
+						//Modify employee
+						case 2:
+							{
+
+								try
+								{
+									Console.WriteLine("Please enter the employee ID");
+									int id1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+									foreach (var obj in employees)
+									{
+
+										if (obj.Key == id1)
+										{
+
+											Console.WriteLine("Employee ID:{0}\nName:{1}\nAge:{2}\nSalary:{3}", obj.Key, obj.Value.Item1, obj.Value.Item2, obj.Value.Item3);
+
+											break;
+										}
+										if (obj.Key != id1)
+										{
+											Console.WriteLine("Please give the correct employee ID that you want to modify");
+											Console.WriteLine("---------------");
+											goto case 2;
+
+
+										}
+
+
+									}
+
+
+									employees.Remove(id1);
+
+									Console.WriteLine("Please enter the updated employee details");
+									em.TakeEmployeeDetailsFromUser();
+									employees.Add(em.Id, Tuple.Create(em.Name, em.Age, em.Salary));
+								}
+								catch (Exception e)
+								{
+									Console.WriteLine("Give EmployeeID as Numbers", e.Message);
+								}
+
+							}
+							break;
+
+
+						//print all employees info
+						case 3:
+							{
+								foreach (var obj in employees)
+								{
+
+									Console.WriteLine("Employee ID:{0}\nName:{1}\nAge:{2}\nSalary:{3}", obj.Key, obj.Value.Item1, obj.Value.Item2, obj.Value.Item3);
+									Console.WriteLine("----------------------------------");
+								}
+
+							}
+							break;
+						//print  specific employee info
+						case 4:
+							{
+								try
+								{
+									Console.WriteLine("Please enter the employee ID");
+									int id1 = Convert.ToInt32(Console.ReadLine());
+
+
+									foreach (var obj in employees)
+									{
+										if (obj.Key == id1)
+										{
+
+											Console.WriteLine("Employee ID:{0}\nName:{1}\nAge:{2}\nSalary:{3}", obj.Key, obj.Value.Item1, obj.Value.Item2, obj.Value.Item3);
+										Console.WriteLine("----------------------------------");
+										break;
+
+										}
+										else
+										{
+											Console.WriteLine("Please give the correct employee ID that you want to See");
+											Console.WriteLine("--------------------");
+											break;
+										}
+
+
+									}
+								}
+								catch (Exception e)
+								{
+									Console.WriteLine("Give EmployeeID as Numbers", e.Message);
+								}
+							}
+							break;
+
+
+						//exit.
+						case 5:
+							{ flag = false; }
+							break;
+
+						default:
+							{
+
+								Console.WriteLine("-----Please give correct input as given above------");
+
+							}
+							break;
+					}
+				}
+		}
+
+
+
+    }
+		
+}
+	
+
